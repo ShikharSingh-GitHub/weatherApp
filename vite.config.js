c81599430f8d7da8b5a32905df510b427bbec501
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-09-03 19:34:07 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-// vite.config.js
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  // Comment out or remove the proxy configuration
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://api.openweathermap.org/data/2.5',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+  base: '/weatherApp/', // This ensures the correct base path for all assets
 });
